09a19a90b935a1466ac8526a8b58748c0cd0d4e9
Veranika_Vayavodzina <veranika1711@gmail.com>
2025-10-02 18:03:49 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,14 @@ export default defineConfig(({ mode }) => {
           entry: resolve(__dirname, "src/lib.tsx"),
           name: "AIChatBot",
           formats: ["es", "umd"],
-          fileName: (format) => `chatbot.${format === "es" ? "js" : "umd.js"}`,
+          // Добавляем хеш к имени файла
+          fileName: (format) => {
+            const timestamp = Date.now();
+            const hash = Math.random().toString(36).substring(2, 8);
+            return `chatbot-${hash}-${timestamp}.${
+              format === "es" ? "js" : "umd.js"
+            }`;
+          },
         },
         rollupOptions: {
           external: ["react", "react-dom"],
@@ -21,10 +28,17 @@ export default defineConfig(({ mode }) => {
               react: "React",
               "react-dom": "ReactDOM",
             },
+            // Хеширование для ассетов
+            assetFileNames: (assetInfo) => {
+              const timestamp = Date.now();
+              const hash = Math.random().toString(36).substring(2, 8);
+              const ext = assetInfo.name?.split(".").pop();
+              const name = assetInfo.name?.replace(`.${ext}`, "");
+              return `${name}-${hash}-${timestamp}.${ext}`;
+            },
           },
         },
         cssCodeSplit: false,
-        assetsInlineLimit: 100000,
       },
     };
   }
